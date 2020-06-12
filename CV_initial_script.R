eacf8a2ed1323320f3fc7b5bc6e224eb1a3a08a0
benedict909 <monstruo319@gmail.com>
2020-06-12 13:34:37 +0100

    Update CV_initial_script.R

--- CV_initial_script.R
+++ CV_initial_script.R
@@ -10,5 +10,5 @@ datadrivencv::use_datadriven_cv(
   data_location = "https://docs.google.com/spreadsheets/d/1WC2VQGbxqxsNcU06x1DMvFBV2AQZg1BCzF26pKs5qCE/edit?usp=sharing",
   # pdf_location = "https://github.com/benedict909/cv/cv.pdf",
   # html_location = "nickstrayer.me/cv/",
-  # source_location = "https://github.com/nstrayer/cv"
+  source_location = "https://github.com/benedict909/CV"
 )
